eacc6468023f881af0026e035572ec9ebecee570
tarikahveci <99441376+tarikahveci@users.noreply.github.com>
2022-12-15 17:40:15 +0300

    Add files via upload

---
+++ ucgen-olusturma.php
@@ -0,0 +1,18 @@
+<?php
+
+function ucgen($say){
+    for($i=0;$i<=$say;$i++){
+
+        $j=0;
+        while($j<=$i){
+            $j++;
+            echo 0 ;
+
+        }
+        echo   "<br>";
+    }
+
+}
+ucgen(15);
+
+?>
